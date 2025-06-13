388cc14f59c92e5140ea63ab41ad202229bc74b6
Jayden Cyril <jaydencyril50@gmail.com>
2025-06-12 19:59:48 -0700

    oncomplete

---
+++ server/models/TrashItem.ts
@@ -0,0 +1,14 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface ITrashItem extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const TrashItemSchema = new Schema<ITrashItem>({
+  content: { type: String, required: true, unique: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const TrashItem = mongoose.model<ITrashItem>('TrashItem', TrashItemSchema);
+export default TrashItem;
