4c0d689c3c595e9d3cf435cb9d0e120b162f6728
Jayden Cyril <jaydencyril50@gmail.com>
2025-06-29 10:32:08 -0700

    a

--- src/services/orderbook.ts
+++ src/services/orderbook.ts
@@ -14,26 +14,26 @@ export const placeOrder = async (
 };
 
 export const cancelOrder = async (orderId: string, token: string) => {
-  const res = await axios.post(`${API}/orderbook/cancel`, { orderId }, { headers: { Authorization: `Bearer ${token}` } });
+  const res = await axios.post(`${API}/api/trade/cancel`, { orderId }, { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
 
 export const fetchOrderBook = async () => {
-  const res = await axios.get(`${API}/orderbook/orderbook`);
+  const res = await axios.get(`${API}/api/trade/orderbook`);
   return res.data;
 };
 
 export const fetchRecentTrades = async () => {
-  const res = await axios.get(`${API}/orderbook/recent`);
+  const res = await axios.get(`${API}/api/trade/recent`);
   return res.data;
 };
 
 export const fetchMyOrders = async (token: string) => {
-  const res = await axios.get(`${API}/orderbook/myorders`, { headers: { Authorization: `Bearer ${token}` } });
+  const res = await axios.get(`${API}/api/trade/myorders`, { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
 
 export const fetchMyTrades = async (token: string) => {
-  const res = await axios.get(`${API}/orderbook/mytrades`, { headers: { Authorization: `Bearer ${token}` } });
+  const res = await axios.get(`${API}/api/trade/mytrades`, { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
