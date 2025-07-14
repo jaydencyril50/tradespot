93448fee4ad6ab5c521ea6c2bb945b5e9591dcf0
Jayden Cyril <jaydencyril50@gmail.com>
2025-07-14 01:19:47 -0700

    a

--- src/pages/TournamentBracket.tsx
+++ src/pages/TournamentBracket.tsx
@@ -1,49 +1,44 @@
 import React from 'react';
 import './TournamentBracket.css';
 
-const leftTopTeams = ['Purple 1', 'Purple 2', 'Purple 3', 'Purple 4', 'Purple 5'];
-const leftBottomTeams = ['Orange 1', 'Orange 2', 'Orange 3', 'Orange 4', 'Orange 5'];
-const rightTopTeams = ['Blue 1', 'Blue 2', 'Blue 3', 'Blue 4', 'Blue 5'];
-const rightBottomTeams = ['Red 1', 'Red 2', 'Red 3', 'Red 4', 'Red 5'];
-
 const TournamentBracket: React.FC = () => (
   <div className="bracket-bg">
     <h2 className="bracket-title">TRADESPOT CHAMPIONS</h2>
     <div className="bracket-main">
 
-      {/* Left Top */}
+      {/* Left Top - all purple */}
       <div className="bracket-side bracket-left">
         <div className="bracket-round round-left round0">
-          {leftTopTeams.map((_, i) => (
+          {Array(5).fill(null).map((_, i) => (
             <div className="bracket-block block-left color-purple" key={i}></div>
           ))}
         </div>
         <div className="bracket-round round-left round1">
-          <div className="bracket-block block-left color-pink"></div>
-          <div className="bracket-block block-left color-pink"></div>
-          <div className="bracket-block block-left color-pink"></div>
+          <div className="bracket-block block-left color-purple"></div>
+          <div className="bracket-block block-left color-purple"></div>
+          <div className="bracket-block block-left color-purple"></div>
         </div>
         <div className="bracket-round round-left round2">
-          <div className="bracket-block block-left color-orange"></div>
-          <div className="bracket-block block-left color-orange"></div>
+          <div className="bracket-block block-left color-purple"></div>
+          <div className="bracket-block block-left color-purple"></div>
         </div>
       </div>
 
-      {/* Left Bottom */}
+      {/* Left Bottom - all yellow */}
       <div className="bracket-side bracket-left">
         <div className="bracket-round round-left round0">
-          {leftBottomTeams.map((_, i) => (
+          {Array(5).fill(null).map((_, i) => (
             <div className="bracket-block block-left color-yellow" key={i}></div>
           ))}
         </div>
         <div className="bracket-round round-left round1">
-          <div className="bracket-block block-left color-orange"></div>
-          <div className="bracket-block block-left color-orange"></div>
-          <div className="bracket-block block-left color-orange"></div>
+          <div className="bracket-block block-left color-yellow"></div>
+          <div className="bracket-block block-left color-yellow"></div>
+          <div className="bracket-block block-left color-yellow"></div>
         </div>
         <div className="bracket-round round-left round2">
-          <div className="bracket-block block-left color-pink"></div>
-          <div className="bracket-block block-left color-pink"></div>
+          <div className="bracket-block block-left color-yellow"></div>
+          <div className="bracket-block block-left color-yellow"></div>
         </div>
       </div>
 
@@ -65,39 +60,39 @@ const TournamentBracket: React.FC = () => (
         <div className="bracket-block semi color-semi-red"></div>
       </div>
 
-      {/* Right Top */}
+      {/* Right Top - all blue */}
       <div className="bracket-side bracket-right">
         <div className="bracket-round round-right round0">
-          {rightTopTeams.map((_, i) => (
+          {Array(5).fill(null).map((_, i) => (
             <div className="bracket-block block-right color-blue" key={i}></div>
           ))}
         </div>
         <div className="bracket-round round-right round1">
-          <div className="bracket-block block-right color-cyan"></div>
-          <div className="bracket-block block-right color-cyan"></div>
-          <div className="bracket-block block-right color-cyan"></div>
+          <div className="bracket-block block-right color-blue"></div>
+          <div className="bracket-block block-right color-blue"></div>
+          <div className="bracket-block block-right color-blue"></div>
         </div>
         <div className="bracket-round round-right round2">
-          <div className="bracket-block block-right color-red"></div>
-          <div className="bracket-block block-right color-red"></div>
+          <div className="bracket-block block-right color-blue"></div>
+          <div className="bracket-block block-right color-blue"></div>
         </div>
       </div>
 
-      {/* Right Bottom */}
+      {/* Right Bottom - all orange */}
       <div className="bracket-side bracket-right">
         <div className="bracket-round round-right round0">
-          {rightBottomTeams.map((_, i) => (
+          {Array(5).fill(null).map((_, i) => (
             <div className="bracket-block block-right color-orange" key={i}></div>
           ))}
         </div>
         <div className="bracket-round round-right round1">
-          <div className="bracket-block block-right color-red"></div>
-          <div className="bracket-block block-right color-red"></div>
-          <div className="bracket-block block-right color-red"></div>
+          <div className="bracket-block block-right color-orange"></div>
+          <div className="bracket-block block-right color-orange"></div>
+          <div className="bracket-block block-right color-orange"></div>
         </div>
         <div className="bracket-round round-right round2">
-          <div className="bracket-block block-right color-cyan"></div>
-          <div className="bracket-block block-right color-cyan"></div>
+          <div className="bracket-block block-right color-orange"></div>
+          <div className="bracket-block block-right color-orange"></div>
         </div>
       </div>
     </div>
