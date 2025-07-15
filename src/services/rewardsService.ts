cb7fb57a83c74e69159e1c1db4f0655cddddc968
Jayden Cyril <jaydencyril50@gmail.com>
2025-07-14 17:39:25 -0700

    a

--- src/services/rewardsService.ts
+++ src/services/rewardsService.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const fetchRewards = async () => {
   const API = process.env.REACT_APP_API_URL || '';
-  const response = await axios.get(`${API}/api/rewards`);
+  const response = await axios.get(`${API}/api/reward/rewards`);
   return response.data.rewards;
 };
